ccf3a1cf106501bba83c2ece9fb3879e42f85d29
Ante Tolušić <ante.tolusic9@gmail.com>
2018-12-02 15:09:03 +0100

    wip

--- src/Color.js
+++ src/Color.js
@@ -3,28 +3,41 @@ import axios from "axios";
 
 class Color extends React.Component {
   state = {
-    colors: ["#000"]
+    colors: [],
+    color: "#000"
   };
   async componentDidMount() {
     const url = "http://www.colr.org/json/color/random";
     const color1 = await axios.get(url);
     const color2 = await axios.get(url);
 
-    this.setState(prevState => ({
-      colors: [
-        ...prevState.colors,
-        color1.data.new_color,
-        color2.data.new_color
-      ]
-    }));
+    this.setState({
+      colors: [color1.data.new_color, color2.data.new_color]
+    });
   }
 
-  onClickHandler = () => {};
+  onClickHandler = () => {
+    const { colors, color } = this.state;
+    if (color === "#000") {
+      this.setState({
+        color: `#${colors[Math.floor(Math.random() * colors.length)]}`
+      });
+    } else {
+      this.setState({ color: "#000" });
+    }
+  };
 
   render() {
+    const { colors } = this.state;
     return (
       <div>
-        <button onClick={e => this.onClickHandler()}>Color</button>
+        <button
+          disabled={colors.length === 0}
+          style={{ color: this.state.color }}
+          onClick={this.onClickHandler}
+        >
+          Color
+        </button>
       </div>
     );
   }
